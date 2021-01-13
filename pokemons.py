df2a67e55ee5fdaa196582c8a5d7738f8f2c8a2c
ParkerXChen <parkerxchen@gmail.com>
2021-01-12 21:56:02 -0500

    Added Wartortle and Blastoise pokemons, changed base catchrate of super rares to 25

--- pokemons.py
+++ pokemons.py
@@ -17,19 +17,19 @@ Bulbasaur = pokemon('1','Bulbasaur','https://professorlotus.com/Sprites/Bulbasau
 
 Ivysaur = pokemon('2','Ivysaur','https://professorlotus.com/Sprites/Ivysaur.gif', ['Grass','Poison'],['Bulbasaur','Ivysaur','Venosaur'], 1, 35, 'r',75,125,25,50)
 
-Venusaur = pokemon('3','Venusaur','https://professorlotus.com/Sprites/Venusaur.gif', ['Grass','Poison'],['Bulbasaur','Ivysaur','Venosaur'], 2, 20, 's',100,150,50,75)
+Venusaur = pokemon('3','Venusaur','https://professorlotus.com/Sprites/Venusaur.gif', ['Grass','Poison'],['Bulbasaur','Ivysaur','Venosaur'], 2, 25, 's',100,150,50,75)
 
 Charmander = pokemon('4','Charmander','https://professorlotus.com/Sprites/Charmander.gif', ['Fire'],['Charmander','Charmeleon','Charizard'], 0, 45, 'u',50,100,10,30)
 
 Charmeleon = pokemon('5','Charmeleon','https://professorlotus.com/Sprites/Charmeleon.gif', ['Fire'],['Charmander','Charmeleon','Charizard'], 1, 35, 'r',75,150,25,50)
 
-Charizard = pokemon('6','Charizard','https://professorlotus.com/Sprites/Charizard.gif', ['Fire'],['Charmander','Charmeleon','Charizard'], 2, 20, 's',100,175,50,75)
+Charizard = pokemon('6','Charizard','https://professorlotus.com/Sprites/Charizard.gif', ['Fire'],['Charmander','Charmeleon','Charizard'], 2, 25, 's',100,175,50,75)
 
 Squirtle = pokemon('7','Squirtle','https://professorlotus.com/Sprites/Squirtle.gif', ['Water'],['Squirtle','Wartortle','Blastoise'], 0, 45, 'u',50,100,10,30)
 
-# Wartortle = pokemon('8','Wartortle','https://professorlotus.com/Sprites/Wartortle.gif', [],[], 1, 20, 's',75,150,75,150)
+Wartortle = pokemon('8','Wartortle','https://professorlotus.com/Sprites/Wartortle.gif', ['Water'],['Squirtle','Wartortle','Blastoise'], 1, 35, 'r',75,150,75,150)
 
-# Blastoise = pokemon('9','Blastoise','https://professorlotus.com/Sprites/Blastoise.gif', [],[], 1, 20, 's',75,150,75,150)
+Blastoise = pokemon('9','Blastoise','https://professorlotus.com/Sprites/Blastoise.gif', ['Water'],['Squirtle','Wartortle','Blastoise'], 1, 25, 's',75,150,75,150)
 
 # Caterpie = pokemon('10','Caterpie','https://professorlotus.com/Sprites/Caterpie.gif', [],[], 1, 20, 's',75,150,75,150)
 
